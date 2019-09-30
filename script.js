53e77592f1d77958b6e6dad8f424e6c7aa27573f
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-30 11:17:07 -0400

    First Class Functions: Passing Functions as Arguments

--- script.js
+++ script.js
@@ -94,6 +94,7 @@
       /**
        * Primitives vs Objects
        */
+      /*
 
        // Primitives
        var a = 23;
@@ -128,3 +129,44 @@
 
        console.log(age);
        console.log(obj.city);
+       */
+      /**
+       * First Class Functions: Passing Functions as Arguments
+       */
+
+       var years = [1990, 1965, 1937, 2005, 1998]
+
+       function arrayCalc(arr, fn) {
+           var arrRes = [];
+           for (var i = 0; i < arr.length; i++) {
+               arrRes.push(fn(arr[i]));
+           }
+           return arrRes;
+       }
+
+       function calculateAge(el) {
+           return 2019 - el;
+       }
+
+       function isFullAge(el) {
+           return el >= 18;
+       }
+
+       function maxHeartRate (el) {
+           if ( el >= 18 && el <= 81) {
+                return Math.round(206.9 - (0.67 * el));
+           } else {
+               return -1;
+           }
+          
+       }
+
+       var ages = arrayCalc(years, calculateAge);
+       var fullAge = arrayCalc(ages, isFullAge);
+       var rates = arrayCalc(ages, maxHeartRate);
+
+       console.log(ages);
+       console.log(fullAge);
+       console.log(rates);
+
+        
